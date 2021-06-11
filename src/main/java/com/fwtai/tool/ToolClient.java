481bdde5dd9addde281a50b13d9ad9bfcf6b2c3b
引路者 <444141300@qq.com>
2021-06-11 14:46:10 +0800

    判断是否异常信息

--- src/main/java/com/fwtai/tool/ToolClient.java
+++ src/main/java/com/fwtai/tool/ToolClient.java
@@ -57,6 +57,9 @@ public final class ToolClient{
   }
 
   public static String queryJson(final Object object){
+    if(object != null && object instanceof Exception){
+      return jsonError();
+    }
     if(object == null || object.toString().trim().length() == 0)
       return jsonEmpty();
     if(object instanceof Map<?,?>){
